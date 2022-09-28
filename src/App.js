fb372052b4e77313bdc5b55ab00ad66b082b3e3d
PiotrFijalkowski <piotrekmen2@wp.pl>
2022-09-28 20:22:26 +0200

    #

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages';
 import Kontakt from './pages/kontakt';
+require('typeface-poppins')
 
 function App() {
 
